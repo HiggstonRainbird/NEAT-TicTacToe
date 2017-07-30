c79c85a41783fec4d48916d1338561283182ae37
HiggstonRainbird <fberg.david@gmail.com>
2017-07-30 16:59:39 -0400

    Uploaded 0.97787 fitness run.

--- Legal Moves/evolve-tictactoe.py
+++ Legal Moves/evolve-tictactoe.py
@@ -21,19 +21,23 @@ import visualize
 
 import random
 
-tictactoe_inputs = [tuple([random.choice([-1,0,1]) for i in range(9)]) for j in range(10)]
+tictactoe_inputs = [tuple([random.choice([-1,0,1]) for i in range(9)]) for j in range(25)]
 tictactoe_outputs = [[int(i==0) for i in board] for board in tictactoe_inputs]
 
 def eval_genomes(genomes, config):
     for genome_id, genome in genomes:
-
+        numberOfBoards = float(len(tictactoe_inputs))
         genome.fitness = 1.0
         net = neat.nn.FeedForwardNetwork.create(genome, config)
+
+        #fitnessBoard = [[0 for j in range(len(tictactoe_inputs))] for i in range(9)]
+
         for xi, xo in zip(tictactoe_inputs, tictactoe_outputs):
             output = net.activate(xi)
 
             for i in range(9):
-                genome.fitness -= float((xo[i] - output[i]) ** 2) / 9.0
+                genome.fitness -= float((xo[i] - output[i]) ** 2) / (9.0 * numberOfBoards)
+
 
 def run(config_file):
     # Load configuration.
@@ -48,7 +52,7 @@ def run(config_file):
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
-    p.add_reporter(neat.Checkpointer(5000-1))
+    p.add_reporter(neat.Checkpointer(1000-1))
 
     # Run for up to 300 generations.
     winner = p.run(eval_genomes, 5000)
@@ -63,7 +67,7 @@ def run(config_file):
         output = winner_net.activate(xi)
         print("input {!r}, expected output {!r}, got {!r}".format(xi, xo, output))
 
-    node_names = {-1:'1,1', -2: '1,2', -3: '1,3', -4: 'D', -5: 'E', -6: 'F', -7: 'G', 0:'Number', 1: 'fizz', 2: 'buzz', 3: 'fizzbuzz'}
+    node_names = {-1:'i 1,1', -2: 'i 1,2', -3: 'i 1,3', -4: 'i 2,1', -5: 'i 2,2', -6: 'i 2,3', -7: 'i 3,1', -8: 'i 3,2', -9: 'i 3,3', 0: 'o 1,1', 1: 'o 1,2', 2: 'o 1,3', 3: 'o 2,1', 4: 'o 2,2', 5: 'o 2,3', 6: 'o 3,1', 7: 'o 3,2', 8: 'o 3,3'}
     visualize.draw_net(config, winner, True, node_names=node_names)
     visualize.plot_stats(stats, ylog=False, view=True)
     #visualize.plot_species(stats, view=True)
